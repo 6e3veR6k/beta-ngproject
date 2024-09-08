af005a3a53e3871ddab8c8eed433684b194c8fb7
Bezvershuk Dmytro <bezvershuk.dmytro@tbibank.bg>
2024-09-08 10:00:38 +0300

    add housing component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,36 @@
 import { Component } from '@angular/core';
 import { FormFieldComponent } from "../shared/components/form-field/form-field.component";
 import { FormsModule } from "@angular/forms";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { HousingLocation } from "../housing-location";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     FormFieldComponent,
-    FormsModule
+    FormsModule,
+    HousingLocationComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  searchInput: { name: string; placeholder: string; type: string } = {
+  readonly baseUrl: string = 'https://angular.dev/assets/iamges/tutorials/common';
+  readonly searchInput: { name: string; placeholder: string; type: string } = {
     name: 'search',
     placeholder: 'Search for something...',
     type: 'text'
+  };
+  housingLocation: HousingLocation = {
+    id: 1,
+    name: 'Dhaka',
+    city: 'Dhaka City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 12,
+    wifi: true,
+    laundry: false,
   };
 
 
